88939d44cb2563d70dc066a7fc99339d397404db
Chris Hodges <chris@chrishodges.me.uk>
2021-09-01 20:04:31 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-cloc (git rev-parse HEAD) | tee -a clocc.txt #get the second last line in the cloc.txt line=( tail -n 2 cloc.txt | head -1 )
+cloc $(git rev-parse HEAD) | tee -a cloc.txt #get the second last line in the cloc.txt line=( tail -n 2 cloc.txt | head -1 )
 #get the code lines SUM
 num=( echo {line} | awk -F" " ‘{ print $5}’)
 #set the sum number as output variable value
