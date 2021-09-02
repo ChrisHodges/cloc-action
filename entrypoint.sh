4c5de0a37956af0e7ab16aca86aa5054a7bd17be
Chris Hodges <chris@chrishodges.me.uk>
2021-09-02 12:33:59 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 export PYTHONIOENCODING=utf8
-cloc --json $(git rev-parse HEAD) | python2 -c "import sys, json; print json.load(sys.stdin)['name']" | tee -a cloc.txt
+cloc --json $(git rev-parse HEAD) | perl -lne 'print $0 if m/sum/;' | tee -a cloc.txt
 
 #get the second last line in the cloc.txt 
 line=$( tail -n 2 cloc.txt | head -1 )
