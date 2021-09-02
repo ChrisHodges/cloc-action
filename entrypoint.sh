e9f484de43f8b433cbe1495b6dd7991ee65dd3eb
Chris Hodges <chris@chrishodges.me.uk>
2021-09-02 12:54:17 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 export PYTHONIOENCODING=utf8
-cloc $(git rev-parse HEAD) | perl -lne 'print "$1\n" if m/SUM:\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)/;' | tee -a cloc.txt
+cloc $(git rev-parse HEAD) | perl -lne 'print "$4\n" if m/SUM:\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)/;' | tee -a cloc.txt
 
 #get the second last line in the cloc.txt 
 line=$( tail -n 2 cloc.txt | head -1 )
