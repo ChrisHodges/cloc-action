ff277fec04b9701ed08a0211d83f6c8c0474e26f
Chris Hodges <chris@chrishodges.me.uk>
2021-09-02 12:29:23 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
-cloc --json $(git rev-parse HEAD) | python3 -c "import sys, json; print(json.load(sys.stdin))" | tee -a cloc.txt
+export PYTHONIOENCODING=utf8
+cloc --json $(git rev-parse HEAD) | python2 -c "import sys, json; print json.load(sys.stdin)['name']" | tee -a cloc.txt
 
 #get the second last line in the cloc.txt 
 line=$( tail -n 2 cloc.txt | head -1 )
