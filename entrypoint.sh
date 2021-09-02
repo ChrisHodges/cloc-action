4386adc65d94f38662301d0b2f46167ca3de2b26
Chris Hodges <chris@chrishodges.me.uk>
2021-09-02 17:08:39 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 cloc $(git rev-parse HEAD) | tee -a cloc.txt
 lines=$(cat cloc.txt | perl -lne 'print "$1\n" if m/SUM:\s+\d+\s+\d+\s+\d+\s+(\d+)/;')
-files=$(cat cloc.txt | perl -lne 'print "$1\n" if m/SUM:\s+(\d)+\s+\d+\s+\d+\s+\d+/;')
+files=$(cat cloc.txt | perl -lne 'print "$1\n" if m/SUM:\s+(\d+)\s+\d+\s+\d+\s+\d+/;')
 echo "::set-output name=lines::${lines}"
 echo "::set-output name=files::${files}"
