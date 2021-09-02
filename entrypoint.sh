144a4bed1cce0b0bc3cd9f29241014798c218002
Chris Hodges <chris@chrishodges.me.uk>
2021-09-02 15:23:19 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env sh
-export PYTHONIOENCODING=utf8
-# cloc $(git rev-parse HEAD) | perl -lne 'print "$2\n" if m/SUM:\s+(\d+)\s+\d+\s+\d+\s+(\d+)/;' | tee -a cloc.txt
 cloc $(git rev-parse HEAD) | tee -a cloc.txt
-cat cloc.txt | perl -lne 'print "$2\n" if m/SUM:\s+(\d+)\s+\d+\s+\d+\s+(\d+)/;' | tee -a cloc.txt
-
-#get the code lines SUM
-num='undefined'
-if grep -oP "SUM:\s+" cloc.txt; then num='Assigned'; fi
-
+num=$(cat cloc.txt | perl -lne 'print "$2\n" if m/SUM:\s+(\d+)\s+\d+\s+\d+\s+(\d+)/;')
 echo "::set-output name=lines::${num}"
