4c7d9f873cf74ada9f4a53a60f49030d310eab61
Chris Hodges <chris@chrishodges.me.uk>
2021-09-02 11:23:05 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,8 @@ line=$( tail -n 2 cloc.txt | head -1 )
 
 #get the code lines SUM
 pat='SUM:\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)'
-[[ $line =~ $pat ]] # $pat must be unquoted
-echo "4: ${BASH_REMATCH[4]}"
-num=$(BASH_REMATCH[4])
+while [[ $line =~ $pat ]]; do
+  num=${BASH_REMATCH[4]}
+done
+
 echo "::set-output name=lines::${num}x"
