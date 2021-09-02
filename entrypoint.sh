6f4ca26e2ebd23e7b3e31b8eb71313f27ed838a9
Chris Hodges <chris@chrishodges.me.uk>
2021-09-02 09:47:27 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,9 @@ cloc $(git rev-parse HEAD) | tee -a cloc.txt
 #get the second last line in the cloc.txt 
 line=$( tail -n 2 cloc.txt | head -1 )
 #get the code lines SUM
-num=$( echo {line} | awk -F" " ‘{ print $5}’)
+#num=$( echo {line} | awk -F" " ‘{ print $5}’)
 #set the sum number as output variable value
 #echo “::set-output name=lines::$num”
 #num="STEEEVE"
+num=`cat cloc.txt`
 echo "::set-output name=lines::${num}x"
